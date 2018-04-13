47c9262a0257d803174c15a9baf4a65589d7a788
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-04-13 20:50:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,8 +42,7 @@ veto (){
   }
 
   sayHi(){
-    return `Hi, my name is ${this.name}.
-    I am from ${this.homeState}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
 
